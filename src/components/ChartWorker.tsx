69f8edac1bcc73f6d4105368b2111c8ab0577caf
daliaelena <dalia.geo89@gmail.com>
2024-01-30 17:41:25 -0600

    hotfix charts

--- src/components/ChartWorker.tsx
+++ src/components/ChartWorker.tsx
@@ -1,6 +1,10 @@
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
-const ChartWorker = ({ dataPoints }) => {
+interface ChartWorkerProps {
+  dataPoints: { x: string; y: number }[];
+}
+
+const ChartWorker: React.FC<ChartWorkerProps> = ({ dataPoints }) => {
   return (
     <BarChart width={600} height={300} data={dataPoints} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
       <XAxis dataKey="x" />
