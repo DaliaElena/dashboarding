aadeef68ba05f9c17cab2c6d4e7ae27e705c5d25
Michi <michi@Michis-MacBook-Pro.local>
2024-02-05 13:51:46 -0600

    ready to deploy

--- src/components/ChartWorker.tsx
+++ src/components/ChartWorker.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid } from 'recharts';
 
 interface ChartWorkerProps {
   dataPoints: { x: string; y: number }[];
@@ -7,11 +7,10 @@ interface ChartWorkerProps {
 
 const ChartWorker: React.FC<ChartWorkerProps> = ({ dataPoints }) => {
   return (
-    <BarChart width={600} height={300} data={dataPoints} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+    <BarChart width={600} height={300} data={dataPoints} margin={{ top: 0, right: 30, left: 20, bottom: 15 }}>
       <XAxis dataKey="x" name="Eje X" label={{ value: "Workers", position: "insideBottom", offset: -10 }} />
       <YAxis name="Eje Y" label={{ value: "Success", angle: -90, position: "insideLeft" }} />
       <CartesianGrid stroke="lightgray" strokeDasharray="2 5" />
-      <Tooltip />
       <Bar dataKey="y" fill="orange" />
     </BarChart>
   );
