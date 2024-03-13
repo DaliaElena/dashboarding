617e98bbec672849d03539f99e87caa374a94c2f
daliaelena <dalia.geo89@gmail.com>
2024-03-13 11:15:09 -0600

    feature help

--- src/pages/Help.tsx
+++ src/pages/Help.tsx
@@ -1,35 +1,56 @@
 import { useState, useEffect } from 'react';
-import marked from 'marked';
-import readmeContent from '../utilities/README.md';
 
-const Help: React.FC = () => {
-  const [markdownContent, setMarkdownContent] = useState<string>('');
+const HelpComponent = () => {
+  const [htmlContent, setHtmlContent] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null); // Especifica el tipo de error
 
   useEffect(() => {
-    fetch(readmeContent)
-      .then(response => {
+    const fetchHtml = async () => {
+      try {
+        const response = await fetch('https://4wfr3j5pz4quixveyazqmwobvm0rqlmi.lambda-url.us-east-1.on.aws/dbos_info');
         if (!response.ok) {
-          throw new Error('Failed to fetch README');
+          throw new Error('Failed to fetch HTML content');
         }
-        return response.text();
-      })
-      .then(data => setMarkdownContent(data))
-      .catch(error => {
-        console.error('Error fetching README:', error);
-        setMarkdownContent('# Error\n\nFailed to load README. Please make sure it exists.');
-      });
+        let html = await response.text();
+        // Remove leading and trailing quotes if present
+        html = html.replace(/^"|"$/g, '');
+        setHtmlContent(html);
+      } catch (error) {
+        if (error instanceof Error) { // Comprobación de tipo
+          setError(error);
+        } else {
+          setError(new Error('An unknown error occurred'));
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchHtml();
   }, []);
 
-  const createMarkup = (content: string) => {
-    return { __html: marked(content) };
-  };
+  if (loading) {
+    return (
+      <div className="column-dbos-center" style={{ minHeight: '100vh', flexDirection: 'column', alignItems: 'center', backgroundColor: '#F9F9F9' }}>
+        Loading...
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="column-dbos-center" style={{ minHeight: '100vh', flexDirection: 'column', alignItems: 'center', backgroundColor: '#F9F9F9' }}>
+        Error: {error.message}
+      </div>
+    );
+  }
 
   return (
-    <div className="help-container">
-      <h2>Help</h2>
-      <div dangerouslySetInnerHTML={createMarkup(markdownContent)} />
+    <div className="column-dbos-center" style={{ minHeight: '100vh', flexDirection: 'column', alignItems: 'center', backgroundColor: '#F9F9F9' }}>
+      <div id="html-container" dangerouslySetInnerHTML={{ __html: htmlContent }} />
     </div>
   );
 };
 
-export default Help;
+export default HelpComponent;
