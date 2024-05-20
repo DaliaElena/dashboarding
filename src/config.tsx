0958f937be1f07bd4992cf41bf2f4da4a7f5176e
Ben <benjamin.casazza@gmail.com>
2024-05-20 10:33:18 -0600

    Update config.tsx

--- src/config.tsx
+++ src/config.tsx
@@ -1,9 +1,9 @@
-export const API_URL_DATA: string = 'https://4wfr3j5pz4quixveyazqmwobvm0rqlmi.lambda-url.us-east-1.on.aws/data';
-export const API_URL_DATA_SOURCE: string = 'https://4wfr3j5pz4quixveyazqmwobvm0rqlmi.lambda-url.us-east-1.on.aws/data_source';
-export const API_URL_CHART: string = 'https://4wfr3j5pz4quixveyazqmwobvm0rqlmi.lambda-url.us-east-1.on.aws/graphworkers';
-export const API_URL_WORKERS: string = 'https://4wfr3j5pz4quixveyazqmwobvm0rqlmi.lambda-url.us-east-1.on.aws/workers';
-export const API_URL_WORKERS_CARDS: string = 'https://4wfr3j5pz4quixveyazqmwobvm0rqlmi.lambda-url.us-east-1.on.aws/workers_cards';
-export const API_URL_DASHBOARDS: string = 'https://4wfr3j5pz4quixveyazqmwobvm0rqlmi.lambda-url.us-east-1.on.aws/dashboards';
-export const API_URL_PIPELINES: string = 'https://4wfr3j5pz4quixveyazqmwobvm0rqlmi.lambda-url.us-east-1.on.aws/pipelines';
+export const API_URL_DATA: string = 'https://wobpf3zx7c647p4qxshfi3v5rm0nlkzs.lambda-url.us-east-1.on.aws/data';
+export const API_URL_DATA_SOURCE: string = 'https://wobpf3zx7c647p4qxshfi3v5rm0nlkzs.lambda-url.us-east-1.on.aws/data_source';
+export const API_URL_CHART: string = 'https://wobpf3zx7c647p4qxshfi3v5rm0nlkzs.lambda-url.us-east-1.on.aws/graphworkers';
+export const API_URL_WORKERS: string = 'https://wobpf3zx7c647p4qxshfi3v5rm0nlkzs.lambda-url.us-east-1.on.aws/workers';
+export const API_URL_WORKERS_CARDS: string = 'https://wobpf3zx7c647p4qxshfi3v5rm0nlkzs.lambda-url.us-east-1.on.aws/workers_cards';
+export const API_URL_DASHBOARDS: string = 'https://wobpf3zx7c647p4qxshfi3v5rm0nlkzs.lambda-url.us-east-1.on.aws/dashboards';
+export const API_URL_PIPELINES: string = 'https://wobpf3zx7c647p4qxshfi3v5rm0nlkzs.lambda-url.us-east-1.on.aws/pipelines';
 export const JUPYTER_INSTANCE: string = 'http://notebook.netjerdbos.cloud/login?next=%2Flab'
-export const DOCUMENTATION: string = 'https://4wfr3j5pz4quixveyazqmwobvm0rqlmi.lambda-url.us-east-1.on.aws/dbos_info'
+export const DOCUMENTATION: string = 'https://wobpf3zx7c647p4qxshfi3v5rm0nlkzs.lambda-url.us-east-1.on.aws/dbos_info'
