72814db819469b6f6496b88557d7d93e0d39dabc
Michi <michi@Michis-MacBook-Pro.local>
2024-12-17 14:39:17 -0600

    replace routes

--- src/components/MenuLatLeft.tsx
+++ src/components/MenuLatLeft.tsx
@@ -22,14 +22,14 @@ const MenuLatLeft: FC<MenuLatLeftProps> = (props) => {
         <div className="principal-menu">
           <span>MENU</span>
             <ListGroup  className="no-list-borders">
-              <ListGroup.Item ><a href="/"><FontAwesomeIcon icon={faHouseChimney} /> Home</a> </ListGroup.Item>
+              <ListGroup.Item ><a href="/dashboarding/"><FontAwesomeIcon icon={faHouseChimney} /> Home</a> </ListGroup.Item>
               <ListGroup.Item ><Dropdown>
                   <Dropdown.Toggle variant="success" id="dropdown-basic">
                     <FontAwesomeIcon icon={faDatabase} /> Data Origin
                   </Dropdown.Toggle>
                   <Dropdown.Menu>
-                    <Dropdown.Item href="/AddNewDataOrigin"><FontAwesomeIcon icon={faSquarePlus} /> Add New Data Origin</Dropdown.Item>
-                    <Dropdown.Item href="/DataOriginHistory"><FontAwesomeIcon icon={faClockRotateLeft} /> Data Origin History</Dropdown.Item>
+                    <Dropdown.Item href="/dashboarding/AddNewDataOrigin"><FontAwesomeIcon icon={faSquarePlus} /> Add New Data Origin</Dropdown.Item>
+                    <Dropdown.Item href="/dashboarding/DataOriginHistory"><FontAwesomeIcon icon={faClockRotateLeft} /> Data Origin History</Dropdown.Item>
                   </Dropdown.Menu>
                 </Dropdown>
               </ListGroup.Item>
@@ -40,23 +40,23 @@ const MenuLatLeft: FC<MenuLatLeftProps> = (props) => {
                   </Dropdown.Toggle>
                   <Dropdown.Menu>
                     <Dropdown.Item href={JUPYTER_INSTANCE}><FontAwesomeIcon icon={faToolbox} /> Add New Worker</Dropdown.Item>
-                    <Dropdown.Item href="/WorkersHistory"><FontAwesomeIcon icon={faBusinessTime} /> Workers History</Dropdown.Item>
+                    <Dropdown.Item href="/dashboarding/WorkersHistory"><FontAwesomeIcon icon={faBusinessTime} /> Workers History</Dropdown.Item>
                   </Dropdown.Menu>
                 </Dropdown>
               </ListGroup.Item>
-              <ListGroup.Item> <a href="/PipelinesHistory"><FontAwesomeIcon icon={faChartLine} />My Pipelines</a></ListGroup.Item> 
-              <ListGroup.Item> <a href="/MyDashboards"><FontAwesomeIcon icon={faTable} /> My Dashboards</a></ListGroup.Item> 
-              <ListGroup.Item> <a href="/AnomalyDetection"><FontAwesomeIcon icon={faChartLine} />Anomaly Detection</a></ListGroup.Item> 
+              <ListGroup.Item> <a href="/dashboarding/PipelinesHistory"><FontAwesomeIcon icon={faChartLine} />My Pipelines</a></ListGroup.Item> 
+              <ListGroup.Item> <a href="/dashboarding/MyDashboards"><FontAwesomeIcon icon={faTable} /> My Dashboards</a></ListGroup.Item> 
+              <ListGroup.Item> <a href="/dashboarding/AnomalyDetection"><FontAwesomeIcon icon={faChartLine} />Anomaly Detection</a></ListGroup.Item> 
           </ListGroup>
         </div>
           {/*OthersMenu*/}
         <div  className="others-menu"> 
           <span>OTHERS</span>
           <ListGroup  className="no-list-borders">
-            <ListGroup.Item  className='hover-effect'><a href="/Settings"><FontAwesomeIcon icon={faGear} /> Settings</a></ListGroup.Item>
-            <ListGroup.Item  className='hover-effect'><a href="/Account"><FontAwesomeIcon icon={faUser} /> Account</a></ListGroup.Item>
-            <ListGroup.Item  className='hover-effect'><a href="/Help"><FontAwesomeIcon icon={faCircleInfo} /> Help</a></ListGroup.Item>
-            <ListGroup.Item className='hover-effect' onClick={props.signOut}><a href=""><FontAwesomeIcon icon={faSignOut} /> Sign Out</a></ListGroup.Item>
+            <ListGroup.Item  className='hover-effect'><a href="/dashboarding/Settings"><FontAwesomeIcon icon={faGear} /> Settings</a></ListGroup.Item>
+            <ListGroup.Item  className='hover-effect'><a href="/dashboarding/Account"><FontAwesomeIcon icon={faUser} /> Account</a></ListGroup.Item>
+            <ListGroup.Item  className='hover-effect'><a href="/dashboarding/Help"><FontAwesomeIcon icon={faCircleInfo} /> Help</a></ListGroup.Item>
+            <ListGroup.Item className='hover-effect' onClick={props.signOut}><a href="/dashboarding"><FontAwesomeIcon icon={faSignOut} /> Sign Out</a></ListGroup.Item>
           </ListGroup>
         </div>
       </div>
