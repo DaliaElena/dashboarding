99adebca0f869c34a273c85b7b8ef71bfede5480
Michi <michi@Michis-MacBook-Pro.local>
2024-12-17 14:39:17 -0600

    replace routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Col, Row } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
 import './App.css';
 import MenuLatLeft from './components/MenuLatLeft';
 import MenuTop from './components/MenuTop';
@@ -19,39 +19,46 @@ import Account from './pages/Account';
 import Help from './pages/Help';
 import PipelinesHistory from './pages/PipelinesHistory';
 
+function Layout() {
+  return (
+    <div style={{ fontFamily: 'Roboto', fontSize: '16px' }}>
+      <Row className="justify-content-start">
+        <Col xs={3} style={{ textAlign: 'left' }}>
+          <MenuLatLeft />
+        </Col>
+        <Col xs={6}>
+          <MenuTop />
+          <Outlet /> {/* Esto renderiza las rutas anidadas */}
+        </Col>
+        <Col xs={3}>
+          <MenuLatRight />
+        </Col>
+      </Row>
+    </div>
+  );
+}
+
 export function App() {
   return (
-    <>
-      <Router>
-        <div style={{ fontFamily: 'Roboto', fontSize: '16px' }}>
-          <Row className="justify-content-start">
-            <Col xs={3} style={{ textAlign: 'left' }}>
-              <MenuLatLeft />
-            </Col>
-            <Col xs={6}>
-              <MenuTop />
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/AddNewWorker" element={<AddNewWorker />} />
-                <Route path="/AddNewDataOrigin" element={<AddNewDataOrigin />} />
-                <Route path="/AddNewDataOriginEditable" element={<AddNewDataOriginEditable />} />
-                <Route path="/DataOriginHistory" element={<DataOriginHistory />} />
-                <Route path="/WorkersHistory" element={<WorkersHistory />} />
-                <Route path="/MyDashboards" element={<MyDashboards />} />
-                <Route path="/AnomalyDetection" element={<AnomalyDetection />} />
-                <Route path="/PipelinesHistory" element={<PipelinesHistory />} />
-                <Route path="/Settings" element={<Settings />} />
-                <Route path="/Account" element={<Account />} />
-                <Route path="/Help" element={<Help />} />
-              </Routes>
-            </Col>
-            <Col xs={3}>
-              <MenuLatRight />
-            </Col>
-          </Row>
-        </div>
-      </Router>
-    </>
+    <Router>
+      <Routes>
+        {/* Ruta base para "dashboarding" */}
+        <Route path="dashboarding" element={<Layout />}>
+          <Route index element={<Home />} /> {/* Ruta base: /dashboarding */}
+          <Route path="AddNewWorker" element={<AddNewWorker />} />
+          <Route path="AddNewDataOrigin" element={<AddNewDataOrigin />} />
+          <Route path="AddNewDataOriginEditable" element={<AddNewDataOriginEditable />} />
+          <Route path="DataOriginHistory" element={<DataOriginHistory />} />
+          <Route path="WorkersHistory" element={<WorkersHistory />} />
+          <Route path="MyDashboards" element={<MyDashboards />} />
+          <Route path="AnomalyDetection" element={<AnomalyDetection />} />
+          <Route path="PipelinesHistory" element={<PipelinesHistory />} />
+          <Route path="Settings" element={<Settings />} />
+          <Route path="Account" element={<Account />} />
+          <Route path="Help" element={<Help />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
