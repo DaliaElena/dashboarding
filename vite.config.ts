0e5aba108f4d70aeb5023f7804923d1de5afd8ac
Michi <michi@Michis-MacBook-Pro.local>
2024-12-22 21:50:16 -0600

    gh pages fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://daliaelena.github.io/dashboarding/',
+  base: '/dashboarding/',
   resolve: {
     alias: {
       './runtimeConfig': './runtimeConfig.browser',
